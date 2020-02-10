ed860c551087da45d4ddb95b307ca2937ea5c3d7
Nina Gautreau <nina.gautreau@gmail.com>
2020-02-10 09:14:48 +0100

    views again

--- models/Tip.js
+++ models/Tip.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const tipSchema = new Schema({
-    body: String,
-    name: String,
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
+    category: {
+        type: String,
+        enum: ["Restaurant", "Bar", "Museum", "Shopping", "Garden", "Info"],
+        required: true
+    },
+    name: {
+        type: String,
         required: true
     },
     country: {
@@ -14,13 +16,20 @@ const tipSchema = new Schema({
         ref: "Country",
         required: true
     },
-    city: String,
-    address: String,
-    category: {
+    city: {
         type: String,
-        enum: ["Restaurant", "Bar", "Museum", "Shopping", "Garden", "Important"],
         required: true
-    }
+    },
+    body: String,
+    address: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
 });
 
 const tipModel = mongoose.model("Tip", tipSchema);
